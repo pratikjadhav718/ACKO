259d63a9859cc83f487480bf3df7aaec59669c07
huxly123 <huxlygerard5@gmail.com>
2021-12-14 13:07:10 +0530

    folder_structure

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import acko from "./ackologo.svg"
 
 function Header() {
     return (
         <div>
-            
+            <img
+            src={acko}
+            ></img>
         </div>
     )
 }
