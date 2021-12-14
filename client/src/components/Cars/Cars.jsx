5a4b665569531b748c30b2eca7f81bd86cbc3c54
huxly123 <huxlygerard5@gmail.com>
2021-12-14 17:07:21 +0530

    cars

--- client/src/components/Cars/Cars.jsx
+++ client/src/components/Cars/Cars.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React from "react";
+import Header from "../Header/Header";
+import style from "./cars.module.css";
 
 function Cars() {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
+  return (
+    <div className={style.carsbody}>
+      <Header />
+      <div
+        style={{
+          fontWeight: "bold",
+          fontSize: "20px",
+          lineHeight: "32px",
+          textAlign: "center",
+          fontStyle: "normal",
+          letterSpacing: "0.02em",
+          marginTop: "80px",
+        }}
+      >
+        Upto 85% Off Comprehensive Car Insurance
+      </div>
+      <div
+        style={{
+          fontStyle: "normal",
+          fontWeight: "700",
+          fontSize: "16px",
+          lineHeight: "26px",
+                  color: "#8A909F",
+          marginTop:"10px"
+        }}
+      >
+        Insure your car in 2 minutes
+      </div>
+    </div>
+  );
 }
 
-export default Cars
+export default Cars;
