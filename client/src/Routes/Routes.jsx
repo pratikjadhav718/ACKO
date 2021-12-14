201044f59a3e8259579d5f86b966e53a049288b1
Antony Chacko <antony101tony@gmail.com>
2021-12-14 16:06:01 +0530

    merged packagelock file

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { DifferentPlanOptions } from "../components/PlanOptions/DiffPlanOptions";
 
 function Routes() {
-    return (
-        <>
-            <Switch>
-                <Route>
-                    <h1>404:Page not found</h1>
-                </Route>
-            </Switch>
-        </>
-    )
+  return (
+    <>
+      <Switch>
+        <Route path="/plans">
+          <DifferentPlanOptions></DifferentPlanOptions>
+        </Route>
+        <Route>
+          <h1>404:Page not found</h1>
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
-export default Routes
+export default Routes;
--- client/src/components/PlanOptions/DiffPlanOptions.jsx
+++ client/src/components/PlanOptions/DiffPlanOptions.jsx
@@ -1,7 +1,3 @@
-
-
-export const DifferentPlanOptions=()=>{
-    return <div>
-
-    </div>
-}
+export const DifferentPlanOptions = () => {
+  return <div>hey there welcome to plans</div>;
+};
