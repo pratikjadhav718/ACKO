0541cc0109b497bd94c917598bde027a0ff5cdb5
pratikjadhav718 <pratikjadhav718@gmail.com>
2021-12-15 16:13:28 +0530

    creating component addtional details.

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { AddtionalDetails } from '../components/AddtionalDetails/AddtionalDetails';
 import Cars from '../components/Cars/Cars'
 import { DifferentPlanOptions } from "../components/PlanOptions/DiffPlanOptions";
 
@@ -16,9 +17,14 @@ function Routes() {
           <DifferentPlanOptions></DifferentPlanOptions>
         </Route>
 
+        <Route path="/addtional-details">
+          <AddtionalDetails />
+        </Route>
+
         <Route>
           <h1>404:Page not found</h1>
         </Route>
+
       </Switch>
     </>
   )
