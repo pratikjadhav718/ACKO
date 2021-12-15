83a1de76a8907d018e8e51412ea2306dc18c20ab
huxly123 <huxlygerard5@gmail.com>
2021-12-15 17:10:04 +0530

    policydone

--- client/src/components/Cars/Lastpolicy/Policy.jsx
+++ client/src/components/Cars/Lastpolicy/Policy.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import style from "./policy.module.css"
+import thumbsup from "./thumbsup paper.svg"
 
 function Policy() {
     return (
-        <div>
-            
-        </div>
-    )
+      <div className={style.policybody} >
+        <div style={{fontStyle:"normal",fontWeight:"bolder",fontSize:"18px",lineHeight:"22px",width:"300px"}} >Did you make a claim in last policy period?</div>
+        <div style={{marginTop:"40px"}} >
+          <button>Yes</button>
+          <button style={{marginLeft:"20px"}} >No</button>
+            </div>
+            <div className={style.policyflexdiv} >
+                <img src={thumbsup} alt='' />
+                <p>You can get amzing discounts basis your claim history</p>
+            </div>
+      </div>
+    );
 }
 
 export default Policy
