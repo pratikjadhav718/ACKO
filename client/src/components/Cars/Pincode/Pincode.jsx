f624f2ae8049e81d7771c341832b2fcddf816f96
huxly123 <huxlygerard5@gmail.com>
2021-12-15 14:57:56 +0530

    pincode

--- client/src/components/Cars/Pincode/Pincode.jsx
+++ client/src/components/Cars/Pincode/Pincode.jsx
@@ -3,8 +3,11 @@ import style from "./pincode.module.css"
 
 function Pincode() {
     return (
-        <div>
-            
+        <div className={style.pincodebody} >
+            <div style={{fontStyle:"normal",fontWeight:"700",fontSize:"20px",lineHeight:"28px"}} >Where do you stay?</div>
+            <input className={style.pininput} placeholder='56005' />
+            <p style={{fontSize:"9px",lineHeight:"20px",fontWeight:"normal",letterSpacing:"0.5px"}} >Please enter the pincode where your vehicle is currently located</p>
+      <div><button className={style.pinbtn} >Continue</button></div>
         </div>
     )
 }
