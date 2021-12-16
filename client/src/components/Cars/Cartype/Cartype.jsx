22973ff3f5f9e76edd610569a77d5897235f3dca
huxly123 <huxlygerard5@gmail.com>
2021-12-16 21:50:33 +0530

    car typehalf

--- client/src/components/Cars/Cartype/Cartype.jsx
+++ client/src/components/Cars/Cartype/Cartype.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import style from "./cartype.module.css"
-
+import car from "./Personel car.svg"
+import petrol from "./petrol.svg"
+import gear from "./gear.svg"
+import downarrow from "./Down arrow.svg"
 function Cartype() {
     return (
-        <div>
-            
+      <div className={style.cartypebody}>
+        <div className={style.cartypehead} >Which Car do you drive?</div>
+        <div className={style.cartypeflex}>
+          <img src={car} alt="" />
+          <p>Ford Ecosport</p>
+          <img src={downarrow} alt="" />
         </div>
-    )
+        <hr />
+        <div className={style.cartypeflex}>
+          <img src={petrol} alt="" />
+          <p>Ford Ecosport</p>
+          <img src={downarrow} alt="" />
+        </div>
+        <hr />
+        <div className={style.cartypeflex}>
+          <img src={gear} alt="" />
+          <p>Ford Ecosport</p>
+          <img src={downarrow} alt="" />
+        </div>
+        <hr />
+        <button className={style.cartypebtn} >Continue</button>
+      </div>
+    );
 }
 
 export default Cartype
