b9ae3d63fd3ed7fc7a6edaa77fd6a73e93917493
huxly123 <huxlygerard5@gmail.com>
2021-12-17 12:35:37 +0530

    carschema

---
+++ server/src/models/car.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+    number: { type: String, required: false },
+    pincode: { type: Number, required: false },
+    year: { type: Number, required: false },
+    month: { type: String, required: false },
+    name: { type: String, required: false },
+    fuel: { type: String, required: false },
+    gear: { type: String, required: false },
+    useofcar: { type: String, required: false },
+    tpdate: { type: String, required: false },
+    oddate:{type:String,required:false}
+})
+
+
+const Car = mongoose.model("car", carSchema);
+
+module.exports=Car
