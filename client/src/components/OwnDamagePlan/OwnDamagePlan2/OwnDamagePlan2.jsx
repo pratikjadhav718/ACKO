223e6a411c41e7e7acbe725ece1521ba6c865afa
pratikjadhav718 <pratikjadhav718@gmail.com>
2021-12-17 23:33:24 +0530

    own damage plan 2

--- client/src/components/OwnDamagePlan/OwnDamagePlan2/OwnDamagePlan2.jsx
+++ client/src/components/OwnDamagePlan/OwnDamagePlan2/OwnDamagePlan2.jsx
@@ -7,9 +7,19 @@ export const OwnDamagePlan2 = () => {
                     <p className="owndamageplan2title">Own Damage Plan</p>
                     <p className="owndamageplan2subtitle">Hide Premium Breakup</p>
             </div>
-            <div>
-                
+            <div className="owndamageplanprices1">
+                <div>
+                    Own Damage Premium
+                </div>
+                <div>₹ 3,650</div>
+
+                <div>NCB Discount</div>
+                <div>- ₹ {"5454"}</div>
+                <div>Consumables Premium</div>
+                <div>₹ 141</div>
             </div>
+
+            
         </div>
     )
 }
