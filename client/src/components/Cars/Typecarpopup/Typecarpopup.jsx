400e223fcf9030b7648261642cae456c020c3840
huxly123 <huxlygerard5@gmail.com>
2021-12-19 01:15:49 +0530

    details backend

--- client/src/components/Cars/Typecarpopup/Typecarpopup.jsx
+++ client/src/components/Cars/Typecarpopup/Typecarpopup.jsx
@@ -3,16 +3,20 @@ import style from "./typecarpopup.module.css"
 import search from "./Search.svg"
 
 
-function Typecarpopup({ setCarpopup, carpopup }) {
+function Typecarpopup({ setCarpopup, carpopup, setCar }) {
   return carpopup ? (
     <div className={style.typecarpopup}>
       <div className={style.inputflexdiv}>
         <img src={search} alt="" />
-        <input type="text" placeholder="Search your car" />
+        <input onChange={(e) => {
+          setCar(e.target.value)
+        }} type="text" placeholder="Search your car" />
       </div>
       <div className={style.topsearch}>Top search results</div>
     </div>
-  ):""
+  ) : (
+    ""
+  );
 }
 
 export default Typecarpopup
