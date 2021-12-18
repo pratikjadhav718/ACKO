2d30591276ed9505b61dabaf7b62f2b5bf831794
huxly123 <huxlygerard5@gmail.com>
2021-12-18 14:44:01 +0530

    online db connected

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect("mongodb://localhost:27017/acko");
+    return mongoose.connect(
+      "mongodb+srv://caddycool:caddycool@cluster0.xarvi.mongodb.net/acko?retryWrites=true&w=majority"
+    );
 }
-
+//mongodb+srv://caddycool:caddycool@cluster0.xarvi.mongodb.net/acko?retryWrites=true&w=majority
+//mongodb://localhost:27017/acko
 module.exports=connect
