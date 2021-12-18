8e9ee6c60b703ea2f3ea54b7211f2d50a391c870
huxly123 <huxlygerard5@gmail.com>
2021-12-18 10:33:22 +0530

    dynamic page

--- client/src/components/Cars/Dynamic/Dynamic.jsx
+++ client/src/components/Cars/Dynamic/Dynamic.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./dynamic.module.css"
+import axios from "axios"
+import buttonpen from "./Button pen.svg"
+import calender from "./Cal.svg"
+import car from "./car.svg"
+import date from "./Date.svg"
+import location from "./Location.svg"
 
 function Dynamic() {
+
+    const id = localStorage.getItem("ackoid");
+
+    const [data,setData]=useState("")
+
+
+    const getData =async () => {
+        await axios.get()
+    }
+
     return (
         <div className={style.dynamicbody} >
             
