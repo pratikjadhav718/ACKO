79d6b1e47fbd25ca867c5c75c970a7c62bec6bd1
huxly123 <huxlygerard5@gmail.com>
2021-12-18 22:10:22 +0530

    pay successfull done

--- client/src/components/Paysuccessfull/Paysuccessfull.jsx
+++ client/src/components/Paysuccessfull/Paysuccessfull.jsx
@@ -2,18 +2,34 @@ import React from 'react'
 import style from "./paysuccessfull.module.css"
 import Header from "../Header/Header"
 import pay from "./paymentgif.png"
+import {Link} from "react-router-dom"
 
 function Paysuccessfull() {
     return (
+      <div>
+        <Header></Header>
         <div>
-            <Header></Header>
-            <img className={style.payimgg} src={pay} alt='' />
-            <button onClick={() => {
+          <img
+            className={style.payimgg}
+            src="https://www.cntraveller.in/wp-content/themes/cntraveller/images/check-circle.gif"
+            alt=""
+          />
+        </div>
+        <div style={{alignItems:"center",marginLeft:"50rem"}} >
+          <Link to="/">
+            <button
+              onClick={() => {
                 localStorage.removeItem("ackoid");
                 localStorage.removeItem("ackoUserId");
-            }} className={style.paybtn} >Continue</button>
+              }}
+              className={style.paybtn}
+            >
+              Continue
+            </button>
+          </Link>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Paysuccessfull
