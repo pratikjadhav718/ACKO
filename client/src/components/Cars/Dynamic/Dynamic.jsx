7a301628241e5808dc0fae5b0a730ab6e9d43f80
huxly123 <huxlygerard5@gmail.com>
2021-12-18 12:08:17 +0530

    dynamic map

--- client/src/components/Cars/Dynamic/Dynamic.jsx
+++ client/src/components/Cars/Dynamic/Dynamic.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "./dynamic.module.css"
 import axios from "axios"
 import buttonpen from "./Button pen.svg"
@@ -11,18 +11,80 @@ function Dynamic() {
 
     const id = localStorage.getItem("ackoid");
 
-    const [data,setData]=useState("")
-
-
+    const [dataaa,setData]=useState([])
+    useEffect(() => {
+    getData()
+},[])
+console.log(dataaa);
     const getData =async () => {
-        await axios.get()
+       const {data}= await axios.get(`http://localhost:8080/cars/${id}`)
+setData([data])
     }
 
     return (
-        <div className={style.dynamicbody} >
-            
-        </div>
-    )
+      <div className={style.dynamicbody}>
+        {dataaa.map((e) => (
+          <div key={e._id}>
+            {e.number && (
+              <div>
+                <img src={car} alt="" />
+                <p>{e.number}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+            {e.pincode && (
+              <div>
+                <img src={location} alt="" />
+                <p>{e.pincode}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+            {e.name && (
+              <div>
+                <img src={car} alt="" />
+                <p>{e.name}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+            {e.oddate && (
+              <div>
+                <img src={date} alt="" />
+                <p>{e.oddate}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+            {e.tpdate && (
+              <div>
+                <img src={date} alt="" />
+                <p>{e.tpdate}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+            {e.year && (
+              <div>
+                <img src={calender} alt="" />
+                <p>{e.year}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+            {e.year && (
+              <div>
+                <img src={calender} alt="" />
+                <p>{e.year}</p>
+                <img src={buttonpen} alt="" />
+              </div>
+            )}
+
+          
+          </div>
+        ))}
+      </div>
+    );
 }
 
 export default Dynamic
